8933df428092e0eacfb7c197d169aa4805265fe8
ThibautJo <thibautjoukes@gmail.com>
2021-05-23 05:22:35 +0200

    Finished header

---
+++ src/components/Navigation.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import WebzzzHeaderNavigationItem from './navItem';
+
+const WebzzzHeaderNavigation = () => {
+  // styles
+  const navStyle = {
+    width: '100%',
+    height: '60px',
+    backgroundColor: 'white',
+    boxShadow: '0px 2px 1px rgba(0,0,0, 0.16)',
+  };
+
+  const navUL = {
+    listStyleType: 'none',
+    margin: '0px',
+    padding: '0px 316px',
+    display: 'flex',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    gap: '60px',
+  };
+
+  // state that hold active nav item
+  const [activeId = 1, setActiveId] = useState();
+
+  return (
+    <nav style={navStyle}>
+      <ul style={navUL}>
+        <WebzzzHeaderNavigationItem
+          clickHandler={setActiveId}
+          id={1}
+          path="#contact"
+          active={activeId === 1 ? 'activeLink' : 'InactiveLink'}
+          itemText="Home"></WebzzzHeaderNavigationItem>
+
+        <WebzzzHeaderNavigationItem
+          clickHandler={setActiveId}
+          id={2}
+          path="#contact"
+          active={activeId === 2 ? 'activeLink' : 'InactiveLink'}
+          itemText="Wie zijn wij?"></WebzzzHeaderNavigationItem>
+
+        <WebzzzHeaderNavigationItem
+          clickHandler={setActiveId}
+          id={3}
+          path="#contact"
+          active={activeId === 3 ? 'activeLink' : 'InactiveLink'}
+          itemText="Wat doen wij?"></WebzzzHeaderNavigationItem>
+
+        <WebzzzHeaderNavigationItem
+          clickHandler={setActiveId}
+          id={4}
+          path="#contact"
+          active={activeId === 4 ? 'activeLink' : 'InactiveLink'}
+          itemText="Contacteer ons"></WebzzzHeaderNavigationItem>
+      </ul>
+    </nav>
+  );
+};
+
+export default WebzzzHeaderNavigation;
---
+++ src/components/navItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './styles.scss';
+
+const WebzzzHeaderNavigationItem = ({ clickHandler, id, active, path, itemText }) => {
+  // styles
+  const listStyle = {
+    fontSize: '16px',
+    paddingBottom: '3px',
+  };
+
+  const anchor = {
+    textDecoration: 'none',
+    color: 'black',
+  };
+
+  return (
+    <li style={listStyle} className={active}>
+      <a href={path} style={anchor} onClick={() => clickHandler(id)} onKeyDown={() => clickHandler(id)}>
+        {itemText}
+      </a>
+    </li>
+  );
+};
+
+export default WebzzzHeaderNavigationItem;
